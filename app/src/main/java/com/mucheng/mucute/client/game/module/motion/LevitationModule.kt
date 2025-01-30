275b6f6ad789f7a237e6e9c00d749fe30363227b
MrPokeG <krosther0@gmail.com>
2025-01-30 13:42:42 +0600

    Levitation implementation

---
+++ app/src/main/java/com/mucheng/mucute/client/game/module/motion/LevitationModule.kt
@@ -0,0 +1,37 @@
+package com.mucheng.mucute.client.game.module.motion
+
+import com.mucheng.mucute.client.game.Module
+import com.mucheng.mucute.client.game.ModuleCategory
+import com.mucheng.mucute.client.game.data.Effect
+import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
+import org.cloudburstmc.protocol.bedrock.packet.MobEffectPacket
+import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
+
+class LevitationModule : Module("levitation", ModuleCategory.Motion) {
+
+    override fun onReceived(packet: BedrockPacket): Boolean {
+        if (packet is PlayerAuthInputPacket && isEnabled) {
+            if (localPlayer.tickExists % 20 == 0L) {
+                session.clientBound(MobEffectPacket().apply {
+                    runtimeEntityId = localPlayer.runtimeEntityId
+                    event = MobEffectPacket.Event.ADD
+                    effectId = Effect.LEVITATION
+                    amplifier = 0
+                    isParticles = false
+                    duration = 21 * 20
+                })
+            }
+        }
+        return false
+    }
+
+    override fun onDisabled() {
+        if (isInGame) {
+            session.clientBound(MobEffectPacket().apply {
+                runtimeEntityId = localPlayer.runtimeEntityId
+                event = MobEffectPacket.Event.REMOVE
+                effectId = Effect.LEVITATION
+            })
+        }
+    }
+}
