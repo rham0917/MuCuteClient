1fd7d165df8854f319c33f8795f656dea7903686
lodingglue <ayushkumar55888@gmail.com>
2025-01-30 21:00:43 +0530

    [+] Fixed Naming

--- app/src/main/java/com/mucheng/mucute/client/game/TranslationManager.kt
+++ app/src/main/java/com/mucheng/mucute/client/game/TranslationManager.kt
@@ -11,7 +11,6 @@ object TranslationManager {
 
     private fun en() = buildMap {
         put("fly", "Fly")
-        put("speed", "Speed")
         put("no_clip", "No Clip")
         put("zoom", "Zoom")
         put("air_jump",  "AirJump")
@@ -22,22 +21,25 @@ object TranslationManager {
         put("levitation", "Levitation")
         put("high_jump", "High Jump")
         put("slow_fall", "Slow Fall")
+        put("anti_knockback","Velocity")
+        put("poseidon","poseidon")
 
     }
 
     private fun zh() = buildMap {
         put("fly", "飞行")
-        put("speed", "速度")
         put("no_clip", "穿墙")
         put("zoom", "缩放")
         put("air_jump",  "空中跳跃")
-        put("speed_hack", "提升速度")
-        put("full_bright", "夜视仪")
+        put("speed_hack", "速度")
+        put("full_bright", "夜视")
         put("haste", "快攻")
         put("jetpack", "喷气背包")
         put("levitate", "悬浮")
-        put("high_jump", "跳高")
-        put("slow_fall", "慢速下落")
+        put("high_jump", "高跳")
+        put("slow_fall", "缓慢下落")
+        put("anti_knockback","防击退")
+        put("poseidon","海神")
     }
 
     fun getTranslationMap(language: String): Map<String, String> {
