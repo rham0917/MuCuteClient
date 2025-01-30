67a6b2d004448811b54e193e8b3675379e1cb35a
MrPokeG <krosther0@gmail.com>
2025-01-30 14:01:24 +0600

    High Jump implementation

---
+++ app/src/main/java/com/mucheng/mucute/client/game/module/motion/HighJumpModule.kt
@@ -0,0 +1,31 @@
+package com.mucheng.mucute.client.game.module.motion
+
+import com.mucheng.mucute.client.game.Module
+import com.mucheng.mucute.client.game.ModuleCategory
+import org.cloudburstmc.protocol.bedrock.data.PlayerAuthInputData
+import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
+import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
+import org.cloudburstmc.protocol.bedrock.packet.SetEntityMotionPacket
+import org.cloudburstmc.math.vector.Vector3f
+
+class HighJumpModule : Module("high_jump", ModuleCategory.Motion) {
+
+    private val jumpHeight = 0.8f  // Higher than normal jump (normal is 0.42f)
+
+    override fun onReceived(packet: BedrockPacket): Boolean {
+        if (packet is PlayerAuthInputPacket && isEnabled) {
+            if (packet.inputData.contains(PlayerAuthInputData.JUMP_DOWN)) {
+                val motionPacket = SetEntityMotionPacket().apply {
+                    runtimeEntityId = localPlayer.runtimeEntityId
+                    motion = Vector3f.from(
+                        localPlayer.motionX,
+                        jumpHeight,
+                        localPlayer.motionZ
+                    )
+                }
+                session.clientBound(motionPacket)
+            }
+        }
+        return false
+    }
+}
