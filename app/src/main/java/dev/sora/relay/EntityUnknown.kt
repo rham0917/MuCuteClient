2f9eab4afd6e889e515a56ff63d8a3ae6d25a266
lodingglue <ayushkumar55888@gmail.com>
2025-02-03 04:25:00 +0530

    [+] Essentials

---
+++ app/src/main/java/dev/sora/relay/EntityUnknown.kt
@@ -0,0 +1,10 @@
+package dev.sora.relay
+
+
+class EntityUnknown(runtimeEntityId: Long, uniqueEntityId: Long, val identifier: String)
+    : Entity(runtimeEntityId, uniqueEntityId) {
+
+    override fun toString(): String {
+        return "EntityUnknown(entityId=$runtimeEntityId, uniqueId=$uniqueEntityId, identifier=$identifier, posX=$posX, posY=$posY, posZ=$posZ)"
+    }
+}
