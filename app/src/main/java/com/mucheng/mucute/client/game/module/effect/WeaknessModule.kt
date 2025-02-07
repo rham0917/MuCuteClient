d68516d324c3b6553a03c4dbcedd353def09fad5
lodingglue <ayushkumar55888@gmail.com>
2025-02-08 03:37:09 +0530

    [+] Weakness

---
+++ app/src/main/java/com/mucheng/mucute/client/game/module/effect/WeaknessModule.kt
@@ -0,0 +1,72 @@
+package com.mucheng.mucute.client.game.module.effect
+
+import com.mucheng.mucute.client.game.Module
+import com.mucheng.mucute.client.game.ModuleCategory
+import com.mucheng.mucute.client.game.data.Effect
+import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
+import org.cloudburstmc.protocol.bedrock.packet.MobEffectPacket
+import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
+import org.cloudburstmc.protocol.bedrock.packet.TextPacket
+
+class WeaknessModule : Module("weakness", ModuleCategory.Effect){
+
+
+
+
+
+
+
+    private val amplifierValue by floatValue("Amplifier", 1f, 1f..5f)
+
+    override fun onEnabled() {
+        if (isSessionCreated) {
+            sendToggleMessage(true)
+        }
+    }
+
+    override fun onDisabled() {
+        if (isSessionCreated) {
+            session.clientBound(MobEffectPacket().apply {
+                runtimeEntityId = session.localPlayer.runtimeEntityId
+                event = MobEffectPacket.Event.REMOVE
+                effectId = Effect.WEAKNESS
+            })
+            sendToggleMessage(false)
+        }
+    }
+
+    private fun sendToggleMessage(enabled: Boolean) {
+        val status = if (enabled) "§aEnabled" else "§cDisabled"
+        val message = "§l§b[MuCute] §r§7Weakness §8» $status"
+
+        val textPacket = TextPacket().apply {
+            type = TextPacket.Type.RAW
+            isNeedsTranslation = false
+            this.message = message
+            xuid = ""
+            sourceName = ""
+        }
+        session.clientBound(textPacket)
+    }
+
+    override fun beforePacketBound(packet: BedrockPacket): Boolean {
+        if (packet is PlayerAuthInputPacket && isEnabled) {
+            if (session.localPlayer.tickExists % 20 == 0L) {
+                session.clientBound(MobEffectPacket().apply {
+                    runtimeEntityId = session.localPlayer.runtimeEntityId
+                    event = MobEffectPacket.Event.ADD
+                    effectId = Effect.WEAKNESS
+                    amplifier = amplifierValue.toInt() - 1
+                    isParticles = false
+                    duration = 360000
+                })
+            }
+        }
+        return false
+    }
+
+
+
+
+
+}
