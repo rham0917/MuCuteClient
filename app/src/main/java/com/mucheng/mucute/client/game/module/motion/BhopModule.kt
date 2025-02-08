78fc01a5dd1d265f4e76bb4a6559de22466db020
lodingglue <ayushkumar55888@gmail.com>
2025-02-08 22:55:59 +0530

    Fix Bhop

--- app/src/main/java/com/mucheng/mucute/client/game/module/motion/BhopModule.kt
+++ app/src/main/java/com/mucheng/mucute/client/game/module/motion/BhopModule.kt
@@ -4,16 +4,22 @@ import com.mucheng.mucute.client.game.InterceptablePacket
 import com.mucheng.mucute.client.game.Module
 import com.mucheng.mucute.client.game.ModuleCategory
 import org.cloudburstmc.math.vector.Vector3f
+import org.cloudburstmc.protocol.bedrock.data.PlayerAuthInputData
+import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
 import org.cloudburstmc.protocol.bedrock.packet.SetEntityMotionPacket
 
 class BhopModule : Module("bhop", ModuleCategory.Motion) {
 
     private val jumpHeight by floatValue("jumpHeight", 0.42f, 0.4f..3.0f)
-    private val motionInterval = 120 // Interval between adjustments in milliseconds
+    private val motionInterval by intValue("Motion interval", 120, 50..2000)
+    private val jumpFactor by intValue("Jumps", 1,1..20)
     private var lastMotionTime = 0L
 
     override fun beforePacketBound(interceptablePacket: InterceptablePacket) {
-        if (!isEnabled) {
+
+        val packet = interceptablePacket.packet
+
+    if (!isEnabled) {
             return
         }
 
@@ -22,19 +28,32 @@ class BhopModule : Module("bhop", ModuleCategory.Motion) {
         // Apply vertical motion adjustments at defined intervals
         if (currentTime - lastMotionTime >= motionInterval) {
             // Apply upward and downward motion to simulate jumping
-            val motionPacket = SetEntityMotionPacket().apply {
-                runtimeEntityId = session.localPlayer.runtimeEntityId
 
-                // Alternate vertical motion to simulate jumping up and down
-                motion = Vector3f.from(
-                    session.localPlayer.motionX,  // Keep horizontal motion
-                    if ((currentTime / motionInterval) % 2 == 0L) jumpHeight else -jumpHeight,  // Alternate between upwards and downwards motion
-                    session.localPlayer.motionZ   // Keep horizontal motion
-                )
+
+
+            if (packet is PlayerAuthInputPacket) {
+
+                if (packet.inputData.contains(PlayerAuthInputData.VERTICAL_COLLISION)) {
+
+
+                    val motionPacket = SetEntityMotionPacket().apply {
+                        runtimeEntityId = session.localPlayer.runtimeEntityId
+
+                        // Alternate vertical motion to simulate jumping up and down
+                        motion = Vector3f.from(
+                            session.localPlayer.motionX,  // Keep horizontal motion
+                            if ((currentTime / (motionInterval / jumpFactor)) % 2 == 0L) jumpHeight else -jumpHeight
+                            ,  // Alternate between upwards and downwards motion
+                            session.localPlayer.motionZ   // Keep horizontal motion
+                        )
+                    }
+
+
+
+                    // Send the motion packet to the server
+                    session.clientBound(motionPacket)
+                }
             }
-
-            // Send the motion packet to the server
-            session.clientBound(motionPacket)
 
             // Update the last motion time
             lastMotionTime = currentTime
