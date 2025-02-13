95e7e7bc82bcd2f4fd35d28b6ec2c3fc03b703a1
lodingglue <ayushkumar55888@gmail.com>
2025-02-13 22:30:46 +0530

    Speed fix

--- app/src/main/java/com/mucheng/mucute/client/game/module/motion/SpeedModule.kt
+++ app/src/main/java/com/mucheng/mucute/client/game/module/motion/SpeedModule.kt
@@ -4,19 +4,15 @@ package com.mucheng.mucute.client.game.module.motion
 import com.mucheng.mucute.client.game.InterceptablePacket
 import com.mucheng.mucute.client.game.Module
 import com.mucheng.mucute.client.game.ModuleCategory
+import org.cloudburstmc.math.vector.Vector3f
+import org.cloudburstmc.protocol.bedrock.data.PlayerAuthInputData
 import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
+import org.cloudburstmc.protocol.bedrock.packet.SetEntityMotionPacket
 
 class SpeedModule : Module("speed", ModuleCategory.Motion) {
 
     private var speedValue by floatValue("speed", 1.3f, 0.1f..5f)
 
-    override fun onDisabled() {
-        val abilities = MotionVarModule.lastUpdateAbilitiesPacket?.clone() ?: return
-
-        abilities.abilityLayers[0].walkSpeed = 0.1f
-        session.clientBound(abilities)
-        super.onDisabled()
-    }
 
     override fun beforePacketBound(interceptablePacket: InterceptablePacket) {
         if (!isEnabled) return
@@ -24,32 +20,22 @@ class SpeedModule : Module("speed", ModuleCategory.Motion) {
         val packet = interceptablePacket.packet
 
         if (packet is PlayerAuthInputPacket) {
-// add option for speeds. ( Vanilla <-> Motion )
-// DEPRECATED: This code is no longer used as it causes undesired behavior where it feels like the player is slipping.
-            // ( multiplication of motion causes velocity to decrease slower than intended )
 
-            // Check if motion is greater than 0.0 and inputData contains VERTICAL_COLLISION
-//            if (packet.motion.length() > 0.0 && packet.inputData.contains(PlayerAuthInputData.VERTICAL_COLLISION)) {
-//
-//                // Create and send the SetEntityMotionPacket with current player's motion
-//                val motionPacket = SetEntityMotionPacket().apply {
-//                    runtimeEntityId = session.localPlayer.runtimeEntityId
-//                    motion = Vector3f.from(
-//                        session.localPlayer.motionX.toDouble() * speedValue,
-//                        session.localPlayer.motionY.toDouble(),
-//                        session.localPlayer.motionZ.toDouble() * speedValue
-//                    )
-//                }
-//
-//                // Send the motion update packet
-//                session.clientBound(motionPacket)
-//            }
-//             This code is used instead to fix the velocity decrease issue as we're setting the speed as expected by the client.
-            val abilities = MotionVarModule.lastUpdateAbilitiesPacket?.clone() ?: return
-            if (speedValue == abilities.abilityLayers[0].walkSpeed) return
+            if (packet.motion.length() > 0.0 && packet.inputData.contains(PlayerAuthInputData.VERTICAL_COLLISION)) {
 
-            abilities.abilityLayers[0].walkSpeed = speedValue
-            session.clientBound(abilities)
+
+                val motionPacket = SetEntityMotionPacket().apply {
+                    runtimeEntityId = session.localPlayer.runtimeEntityId
+                    motion = Vector3f.from(
+                        session.localPlayer.motionX.toDouble() * speedValue,
+                        session.localPlayer.motionY.toDouble(),
+                        session.localPlayer.motionZ.toDouble() * speedValue
+                    )
+                }
+
+
+                session.clientBound(motionPacket)
+            }
         }
     }
 }
